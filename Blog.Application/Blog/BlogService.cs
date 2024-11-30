e4744c06e5c0418c480d499eefad0a06df3707c8
Jitendra Bansal <162089979+JitendraRS@users.noreply.github.com>
2024-11-30 23:55:44 +0530

    Added Core and Application layers Implemented CRUD operation for blogs ang global exception handler.

---
+++ Blog.Application/Blog/BlogService.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Core.Blog;
+
+namespace Blog.Application.Blog;
+
+public class BlogService : IBlogService
+{
+    private readonly IBlogRepository _blogRepository;
+    public BlogService(IBlogRepository blogRepository)
+    {
+        _blogRepository = blogRepository;
+    }
+
+    public async Task<Core.Blog.Blog> CreateBlog(Core.Blog.Blog blog)
+    {
+       return await _blogRepository.CreateBlog(blog);
+    }
+
+    public async Task<bool> DeleteBlog(int id)
+    {
+        return await _blogRepository.DeleteBlog(id);
+    }
+
+    public async Task<List<Core.Blog.Blog>> GetAll()
+    {
+        return await _blogRepository.GetAll();
+    }
+
+    public async Task<Core.Blog.Blog?> GetBlogById(int id)
+    {
+       return await _blogRepository.GetBlogById(id);
+    }
+
+    public async Task<bool> UpdateBlog(int id, Core.Blog.Blog updatedBlog)
+    {
+       return await _blogRepository.UpdateBlog(id, updatedBlog);
+    }
+}
---
+++ Blog.WebAPI/Infrastructure/ExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mime;
+
+namespace Blog.WebAPI.Infrastructure;
+public class ExceptionHandler
+{
+    private readonly RequestDelegate _requestDelegate;
+
+    public ExceptionHandler(RequestDelegate requestDelegate)
+    {
+        _requestDelegate = requestDelegate;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _requestDelegate(context);
+        }
+        catch (Exception e)
+        {
+            await HandleException(context, e);
+        }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex) 
+    {
+        var response = context.Response;
+
+        response.Clear();
+        response.StatusCode = StatusCodes.Status500InternalServerError;
+        response.ContentType = MediaTypeNames.Application.Json;
+
+        var error = new
+        {
+            Message = "An unexpected error occurred. Please try again later",
+            ErrorCode = StatusCodes.Status500InternalServerError,
+            Error = ex.Message,
+        };
+
+        await response.WriteAsJsonAsync(error);
+    }
+}
