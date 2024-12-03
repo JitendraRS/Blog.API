fb295fd3a898553312fab163432e65c95dae47d2
Jitendra Bansal <162089979+JitendraRS@users.noreply.github.com>
2024-12-03 10:37:40 +0530

    added xml comments

--- Blog.WebAPI/Infrastructure/ExceptionHandler.cs
+++ Blog.WebAPI/Infrastructure/ExceptionHandler.cs
@@ -1,15 +1,27 @@
 ﻿using System.Net.Mime;
 
 namespace Blog.WebAPI.Infrastructure;
+/// <summary>
+/// Middleware to handle exceptions that occur during the request processing pipeline.
+/// </summary>
 public class ExceptionHandler
 {
     private readonly RequestDelegate _requestDelegate;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExceptionHandler"/> class.
+    /// </summary>
+    /// <param name="requestDelegate">The next middleware in the request pipeline.</param>
     public ExceptionHandler(RequestDelegate requestDelegate)
     {
         _requestDelegate = requestDelegate;
     }
 
+    /// <summary>
+    /// Invokes the middleware to handle exceptions.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>A task that represents the completion of request processing.</returns>
     public async Task Invoke(HttpContext context)
     {
         try
@@ -22,7 +34,13 @@ public class ExceptionHandler
         }
     }
 
-    private async Task HandleException(HttpContext context, Exception ex) 
+    /// <summary>
+    /// Handles the exception by setting the response status code and writing the error details to the response.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <param name="ex">The exception that occurred.</param>
+    /// <returns>A task that represents the completion of the exception handling.</returns>
+    private async Task HandleException(HttpContext context, Exception ex)
     {
         var response = context.Response;
 
